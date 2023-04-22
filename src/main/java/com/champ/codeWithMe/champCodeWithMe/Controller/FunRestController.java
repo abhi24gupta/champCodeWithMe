e5713009a2c84b4577d3442ec5755573cc8a9391
Abhinav Gupta <201851003@iiitvadodara.ac.in>
2023-04-22 16:33:21 +0530

    App 2.1 || Automated the compiling and Using Application.propeties

--- src/main/java/com/champ/codeWithMe/champCodeWithMe/Controller/FunRestController.java
+++ src/main/java/com/champ/codeWithMe/champCodeWithMe/Controller/FunRestController.java
@@ -1,15 +1,27 @@
 package com.champ.codeWithMe.champCodeWithMe.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    private static final Logger log = LoggerFactory.getLogger(FunRestController.class);
+
+
+    @Value("${home.name}")
+    private String name;
+
+    @Value("${home.city}")
+    private String city;
     // expose "/" that return "Hello World"
 
     @GetMapping("/get/home")
     public String sayHello(){
+        log.info("Hi, I am {} and I live in {}",name,city);
         return "Hello World";
     }
 }
