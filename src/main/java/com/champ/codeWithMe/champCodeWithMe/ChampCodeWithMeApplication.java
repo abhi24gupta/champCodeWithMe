369ecdec445b4dfe842912560245327fabef0b9e
Abhinav Gupta <201851003@iiitvadodara.ac.in>
2023-04-28 01:33:10 +0530

    App 5.1|HIbernate Section3 Lecture 61

--- src/main/java/com/champ/codeWithMe/champCodeWithMe/ChampCodeWithMeApplication.java
+++ src/main/java/com/champ/codeWithMe/champCodeWithMe/ChampCodeWithMeApplication.java
@@ -2,9 +2,11 @@ package com.champ.codeWithMe.champCodeWithMe;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ChampCodeWithMeApplication {
@@ -16,6 +18,13 @@ public class ChampCodeWithMeApplication {
 		log.info("Application is Up");
 
 		SpringApplication.run(ChampCodeWithMeApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+			System.out.println("Hello World");
+		};
 	}
 
 }
--- src/main/java/com/champ/codeWithMe/champCodeWithMe/Controller/FunRestController.java
+++ src/main/java/com/champ/codeWithMe/champCodeWithMe/Controller/FunRestController.java
@@ -2,8 +2,6 @@ package com.champ.codeWithMe.champCodeWithMe.Controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,27 +9,5 @@ public class FunRestController {
 
     private static final Logger log = LoggerFactory.getLogger(FunRestController.class);
 
-
-    @Value("${home.name}")
-    private String name;
-
-    @Value("${home.city}")
-    private String city;
-    // expose "/" that return "Hello World"
-
-    @GetMapping("/get/home")
-    public String sayHello(){
-        log.info("Hi, I am {} and I live in {}",name,city);
-        return "Hello World";
-    }
-    @GetMapping("/workout")
-    public String getDailyWorkout(){
-        return "Let's begin to complete this course";
-    }
-
-    @GetMapping("/fortune")
-    public String getDailyFortune(){
-        return "Today is you lucky day";
-    }
 
 }
